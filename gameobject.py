77e1427a17cce78d580ecf4f4ec259c74ef97665
Mikael <lightspeed1001@gmail.com>
2019-10-09 00:22:00 +0000

    herp derp cleanup

--- gameobject.py
+++ gameobject.py
@@ -6,7 +6,14 @@ class GameObject:
         self.scale = scale
         self.rotation = rotation
         self.friction = friction
-        
+        self.update_bounding_box()
+
+    def intersects(self, other):
+        return (self.min_x <= other.max_x and self.max_x >= other.min_x) and \
+               (self.min_y <= other.max_y and self.max_y >= other.min_y) and \
+               (self.min_z <= other.max_z and self.max_z >= other.min_z)
+
+    def update_bounding_box(self):
         self.max_x = self.position.x + self.scale.x / 2
         self.max_y = self.position.y + self.scale.y / 2
         self.max_z = self.position.z + self.scale.z / 2
@@ -14,12 +21,7 @@ class GameObject:
         self.min_x = self.position.x - self.scale.x / 2
         self.min_y = self.position.y - self.scale.y / 2
         self.min_z = self.position.z - self.scale.z / 2
-
-    def intersects(self, other):
-        return (self.min_x <= other.max_x and self.max_x >= other.min_x) and \
-               (self.min_y <= other.max_y and self.max_y >= other.min_y) and \
-               (self.min_z <= other.max_z and self.max_z >= other.min_z)
-
+        
     def collision_check(self, other):
         pass
 
@@ -30,51 +32,33 @@ class Player(GameObject):
         self.direction = None
         self.acceleration = acceleration
         self.radius = self.scale.x / 2
-
+    
     def collision_check(self, other):
         # Just check if player is inside the object and eject him
         
         # Update player bounding box first
-        self.max_x = self.position.x + self.scale.x / 2
-        self.max_y = self.position.y + self.scale.y / 2
-        self.max_z = self.position.z + self.scale.z / 2
-
-        self.min_x = self.position.x - self.scale.x / 2
-        self.min_y = self.position.y - self.scale.y / 2
-        self.min_z = self.position.z - self.scale.z / 2
+        self.update_bounding_box()
 
         if self.intersects(other):
             # Bounding boxes do collide. Time to eject the player properly.
+            # TODO Add y collision check for gravity
+            
             w = other.position.x - self.position.x
             h = other.position.z - self.position.z
-
-            if( w >= abs(h)): 
+            
+            if( w >= abs(h)):
                 self.position.x = other.min_x - self.scale.x / 2
-            if(-w >= abs(h)): 
+                return
+            if(-w >= abs(h)):
                 self.position.x = other.max_x + self.scale.x / 2
-            if( h >= abs(w)): 
+                return
+            if( h >= abs(w)):
                 self.position.z = other.min_z - self.scale.z / 2
-            if(-h >= abs(w)): 
+                return
+            if(-h >= abs(w)):
                 self.position.z = other.max_z + self.scale.z / 2
-            # var w = wall.Center.X - player.Center.X;
-            # var h = wall.Center.Y - player.Center.Y;
-
-            # if (w >= Math.Abs(h)) player.Center = new Point(wall.MinX - player.Size.Width / 2, player.Center.Y);
-            # if (-w >= Math.Abs(h)) player.Center = new Point(wall.MaxX + player.Size.Width / 2, player.Center.Y);
-            # if (h >= Math.Abs(w)) player.Center = new Point(player.Center.X, wall.MinY - player.Size.Height / 2);
-            # if (-h >= Math.Abs(w)) player.Center = new Point(player.Center.X, wall.MaxY + player.Size.Height / 2);
-
-
-            # if(self.max_z > other.min_z):
-            #     # print("this one?")
-            #     self.position.z = other.max_z + self.scale.z / 2
-            #     return True
-
-            # # This guy never gets called :(
-            # if(self.min_z > other.max_z):
-            #     print("other one?")
-            #     self.position.z = other.min_z - self.scale.z / 2
-            #     return True
+                return
+            print("min_x: {}; max_x: {}; min_y: {}; max_y: {}; min_z: {}; max_xz {};".format(self.min_x, self.max_x, self.min_y, self.max_y, self.min_z, self.max_z))
 
             # if( self.position.z > other.position.z and
             #     self.position.x > other.min_z and
@@ -83,8 +67,6 @@ class Player(GameObject):
             #         return True
     
     def sphere_intersects(self, other):
-        # other scale: 5 5 5
-        # self scale: 0.75 0.75 0.75
         x = max(other.position.x - other.scale.x / 2, min(self.position.x, (other.position.x + other.scale.x / 2)))
         y = max(other.position.y - other.scale.y / 2, min(self.position.y, (other.position.y + other.scale.y / 2)))
         z = max(other.position.z - other.scale.z / 2, min(self.position.z, (other.position.z + other.scale.z / 2)))
